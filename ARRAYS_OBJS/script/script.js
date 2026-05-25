2467c8a5da4c9c9439eb40b1eac8079ad9062802
GuilhermeCosta97 <167794185+GuilhermeCosta97@users.noreply.github.com>
2026-05-25 13:29:11 -0300

    arrays

---
+++ ARRAYS_OBJS/script/script.js
@@ -0,0 +1,32 @@
+// Arrays
+
+const lista = [1, 2, 3, 4, 5]
+
+console.log(lista)
+
+console.log(typeof lista)
+
+const itens = ["Matheus", true, 2, 5.25, []]
+
+console.log(itens)
+
+// Mais sobre arrays
+
+const arr = ["a", "b", "c"]
+
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[83])
+
+// Propriedades 
+
+const number = [5, 5, 6]
+
+console.log(number.length)
+
+console.log(number[`length`])
+
+const myName = "Guilherme"
+
+console.log(myName.length) 
