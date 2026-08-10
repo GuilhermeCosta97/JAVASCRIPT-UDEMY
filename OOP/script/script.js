75f59fe49f38867dde6dafecfd59ace86774f4a0
GuilhermeCosta97 <167794185+GuilhermeCosta97@users.noreply.github.com>
2026-08-10 13:51:05 -0300

    update.script

--- OOP/script/script.js
+++ OOP/script/script.js
@@ -33,3 +33,37 @@ console.log(pessoa.getName())
 pessoa.setName("Gus")
 
 console.log(pessoa.getName())
+
+// Prototype
+
+const text = "asd"
+
+console.log(Object.getPrototypeOf(text))
+
+const bool = true
+
+console.log(Object.getPrototypeOf(bool))
+
+const arr = []
+
+console.log(arr.length)
+
+console.log(Object.getPrototypeOf(arr) )
+
+// Mais sobre Prototype
+
+const myObjetct = {
+    a: "B",
+}
+
+console.log(Object.getPrototypeOf(myObjetct))
+
+console.log(Object.getPrototypeOf(myObjetct) === (Object.prototype)) 
+
+const mySecondObject = Object.create(myObjetct)
+
+console.log(mySecondObject)
+
+console.log(mySecondObject.a)
+
+console.log(Object.getPrototypeOf(mySecondObject) === myObjetct)
