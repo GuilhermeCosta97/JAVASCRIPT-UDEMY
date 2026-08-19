45df5eab904f3185816cf9342e322889e4a017e5
GuilhermeCosta97 <167794185+GuilhermeCosta97@users.noreply.github.com>
2026-08-19 19:18:33 -0300

    update script

--- DEBUG/script/script.js
+++ DEBUG/script/script.js
@@ -31,3 +31,93 @@ if(a > 5) {
 }
 
 console.log(a)
+
+// 3 - Debugger
+
+let c = 1
+let d = 2
+
+if(d == 2) {
+    c = c + 2
+}
+
+// debugger;
+
+for (let i = 0; i < d; i++ ) {
+    c = c + 2
+}
+
+console.log("Executou o loop")
+
+// debugger;
+
+if (d == 2) {
+    d = d + 2
+}
+
+// 4 - Tratamento de dados
+function checkNumber(n) {
+
+    const result = Number(n)
+
+    if (Number.isNaN(result)) {
+        console.log("Valor incorreto")
+        return;
+    }
+
+    console.log("Valor correto")
+    return result;
+
+}
+
+checkNumber(5)
+checkNumber("Teste")
+checkNumber("10")
+checkNumber({})
+
+// 5 - exception
+
+let x = 10
+
+if(x != 11) {
+    // throw new Error("O valor nao pode ser diferente de 11");
+}
+
+// 6 - Try catch
+
+try {
+    const soma = x + y
+} catch (error) {
+    console.log(`Erro no programa ${error}`)
+}
+
+// 7 - finally
+
+try {
+    const value = checkNumber("asd")
+
+    if(!value) {
+        throw new Error("Valores invalidos!")
+    }
+
+} catch (error) {
+    console.log(`Opa aconteceu um problema: ${error}`)
+} finally {
+    console.log("O código foi executado!")
+}
+
+// 8 - Assertion 
+
+function checkArray(arr) {
+
+    if(arr.length === 0) {
+        throw new Error("O array precisa ter elementos")
+    } else {
+        console.log(`O array tem ${arr.length} elementos`)
+    }
+
+}
+
+// checkArray([])
+
+checkArray([1, 2, 3])
