4e93a1d4485507180e757466c554dc372bcdedda
GuilhermeCosta97 <167794185+GuilhermeCosta97@users.noreply.github.com>
2026-08-24 13:35:04 -0300

    section dom create

---
+++ DOM/js/scipt.js
@@ -0,0 +1,11 @@
+// 1 - Movimentando-se pelo DOM
+
+console.log(document.body)
+
+console.log(document.body.childNodes)
+
+console.log(document.body.childNodes[1])
+
+console.log(document.body.childNodes[1].childNodes)
+
+console.log(document.body.childNodes[1].childNodes[1].textContent)
