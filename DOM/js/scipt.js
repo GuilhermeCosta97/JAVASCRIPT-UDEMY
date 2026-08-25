ce23e731d4c523e24dacd778c0ef41e24bbf3fa8
GuilhermeCosta97 <167794185+GuilhermeCosta97@users.noreply.github.com>
2026-08-25 14:18:31 -0300

    update script

--- DOM/js/scipt.js
+++ DOM/js/scipt.js
@@ -9,3 +9,31 @@ console.log(document.body.childNodes[1])
 console.log(document.body.childNodes[1].childNodes)
 
 console.log(document.body.childNodes[1].childNodes[1].textContent)
+
+// 2 - Selecionando elementos por Tags
+
+const listItens = document.getElementsByTagName("li")
+
+console.log(listItens)
+
+// 3 - Selecionando elementos por ID
+
+const title = document.getElementById("title")
+
+console.log(title)
+
+// 4 - Selecionando elementos por Classes
+
+const product = document.getElementsByClassName("product")
+
+console.log(product)
+
+// 5 - Selecionando elementos por CSS
+
+const productQuery = document.querySelectorAll(".product")
+
+console.log(productQuery)
+
+const mainContainer = document.querySelector("#main-container")
+
+console.log(mainContainer)
