6df6c68c02382bfb1d5b21745bccea71d6a1114f
GuilhermeCosta97 <167794185+GuilhermeCosta97@users.noreply.github.com>
2026-09-01 13:51:52 -0300

    update script

--- EVENTOS/js/script.js
+++ EVENTOS/js/script.js
@@ -5,3 +5,31 @@ const btn = document.querySelector("#my-button")
 btn.addEventListener("click", function() {
     console.log("Clicou aqui !")
 })
+
+// 2 - Removendo evento
+
+const secondBtn = document.querySelector("#btn")
+
+function imprimirMensagem() {
+    console.log("teste")
+}
+
+secondBtn.addEventListener("click", imprimirMensagem)
+
+const thirdBtn = document.querySelector("#other-btn")
+
+thirdBtn.addEventListener("click", () => {
+    console.log("Evento removido")
+    secondBtn.removeEventListener("click", imprimirMensagem)
+})
+
+// 3 - Argumento do evento
+
+const myTitle = document.querySelector("#my-title")
+
+myTitle.addEventListener("click", (event) => {
+    console.log(event) 
+    console.log(event.offsetX) 
+    console.log(event.pointerType)
+    console.log(event.target)   
+})
