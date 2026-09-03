ae5327939792e43b4c46dff44e5caff224e66691
GuilhermeCosta97 <167794185+GuilhermeCosta97@users.noreply.github.com>
2026-09-03 13:46:16 -0300

    update script

--- EVENTOS/js/script.js
+++ EVENTOS/js/script.js
@@ -33,3 +33,38 @@ myTitle.addEventListener("click", (event) => {
     console.log(event.pointerType)
     console.log(event.target)   
 })
+
+// 4 - Propagando eventos
+
+const btnContainer = document.querySelector("#btn-container")
+const btnInsiderContainer = document.querySelector("#div-btn")
+
+btnContainer.addEventListener("click", () => {
+    console.log("Evento 1")
+})
+
+btnInsiderContainer.addEventListener("click", (e) => {
+    e.stopImmediatePropagation()
+    console.log("Evento 2")
+})
+
+// 5 - Removendo evento padrão
+
+const a = document.querySelector("a")
+
+a.addEventListener("click", (e) => {
+
+    e.preventDefault()
+
+    console.log("Não alterou a pagina")
+})
+
+// 6 - Eventos de tecla
+
+document.addEventListener("keyup", (e) => {
+    console.log(`Soltou a tecla ${e.key}`)
+})
+
+document.addEventListener("keydown", (e) => {
+    console.log(`Presionou a tecla ${e.key}`)
+})
